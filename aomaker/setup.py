d76059e227c56ae0805bea85b09c12da2ce9e5fa
luxinyu <414903281@qq.com>
2024-12-10 10:54:04 +0800

    增加setup

---
+++ aomaker/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aomaker',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # 项目的依赖库，例如
+        # 'requests', 'numpy',
+    ],
+)
